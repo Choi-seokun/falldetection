e322a5590c08569448f460cf4b032dcffa25c310
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2024-12-09 00:18:32 +0900

    Add files via upload

---
+++ src/main/java/falldetection/spring/Domain/User.java
@@ -0,0 +1,27 @@
+package falldetection.spring.Domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String identifier;
+
+    private String password;
+
+    private String name;
+
+    private String phone_num;
+}
